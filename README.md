ac45d4631813dd3c9c13570fad74e8f5f63efb19
Julien LEICHER <jleicher@gmail.com>
2017-12-01 10:16:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Cerebro-Qwant
+
+> A Cerebro plugin to look up Qwant
+
+*Based on the idea from [Cerebro-Google](https://github.com/KELiON/cerebro-google)*
+
+![](screenshot.png)
+
+## Usage
+Type any word you'd like to search!
+
+## Features
+
+* Get related content suggestions from Qwant
+
+## TODO
+
+- [ ] Cleanup the code
+- [ ] Maybe use Qwant previews
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Loading, KeyboardNav, KeyboardNavItem } from 'cerebro-ui'
+import debounce from './debounce';
+import styles from './styles.css'
+
+const suggest = (suggestTerm) => {
+  return fetch(`https://api.qwant.com/api/suggest?q=${suggestTerm}`)
+    .then(r => r.json())
+    .then(d => d.data.items.map(o => o.value));
+};
+
+class Preview extends React.Component {
+  constructor() {
+    super();
+    this.state = { items: [], loading: false };
+    this.retrieveSuggestionsUpdate = debounce(() => this.retrieveSuggestions(), 500);
+  }
+
+  retrieveSuggestions() {
+    this.setState({ loading: true });
+    
+    suggest(this.props.query).then((items) => this.setState({
+      items,
+      loading: false,
+    }));
+  }
+
+  componentDidMount() {
+    this.retrieveSuggestions();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.query !== this.props.query) {
+      this.retrieveSuggestionsUpdate();
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        {this.state.loading ? 
+        <Loading /> :
+        <KeyboardNav>
+          <ul className={styles.list}>
+          {
+            this.state.items.map(s => (
+              <KeyboardNavItem
+                key={s}
+                tagName={'li'}
+                onSelect={() => this.props.onSelected(s)}
+              >
+                {s}
+              </KeyboardNavItem>
+            ))
+          }
+          </ul>
+        </KeyboardNav>}
+      </div>
+      );
+  }
+}
+
+export const fn = ({ term, actions, display }) => {
+
+  const search = (searchTerm) => {
+    const q = encodeURIComponent(searchTerm)
+    actions.open(`https://www.qwant.com/?q=${q}&t=all`)
+    actions.hideWindow()
+  }
+
+  // Put your plugin code here
+  display({
+    title: `Search qwant for ${term}`,
+    icon: 'https://www.qwant.com/favicon.png',
+    onSelect: () => search(term),
+    getPreview: () => <Preview query={term} onSelected={search} />,
+  })
+}
